6936abbae991371d03f186c768ccf696702ff5d2
erdai <peachorchard@qq.com>
2012-10-19 16:08:00 +0800

    aitacoffee layout
    Signed-off-by: erdai <peachorchard@qq.com>

---
+++ src/com/ostocy/yooxi/holder/YooxiCoffeeIndexViewHolder.java
@@ -0,0 +1,65 @@
+package com.ostocy.yooxi.holder;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class YooxiCoffeeIndexViewHolder {
+	private ImageView lefttopimage;
+	private ImageView righttopimage;
+	private ImageView leftbottomimage;
+	private ImageView rightbottomimage;
+	private TextView lefttoptext;
+	private TextView righttoptext;
+	private TextView leftbottomtext;
+	private TextView rightbottomtext;
+	public ImageView getLefttopimage() {
+		return lefttopimage;
+	}
+	public void setLefttopimage(ImageView lefttopimage) {
+		this.lefttopimage = lefttopimage;
+	}
+	public ImageView getRighttopimage() {
+		return righttopimage;
+	}
+	public void setRighttopimage(ImageView righttopimage) {
+		this.righttopimage = righttopimage;
+	}
+	public ImageView getLeftbottomimage() {
+		return leftbottomimage;
+	}
+	public void setLeftbottomimage(ImageView leftbottomimage) {
+		this.leftbottomimage = leftbottomimage;
+	}
+	public ImageView getRightbottomimage() {
+		return rightbottomimage;
+	}
+	public void setRightbottomimage(ImageView rightbottomimage) {
+		this.rightbottomimage = rightbottomimage;
+	}
+	public TextView getLefttoptext() {
+		return lefttoptext;
+	}
+	public void setLefttoptext(TextView lefttoptext) {
+		this.lefttoptext = lefttoptext;
+	}
+	public TextView getRighttoptext() {
+		return righttoptext;
+	}
+	public void setRighttoptext(TextView righttoptext) {
+		this.righttoptext = righttoptext;
+	}
+	public TextView getLeftbottomtext() {
+		return leftbottomtext;
+	}
+	public void setLeftbottomtext(TextView leftbottomtext) {
+		this.leftbottomtext = leftbottomtext;
+	}
+	public TextView getRightbottomtext() {
+		return rightbottomtext;
+	}
+	public void setRightbottomtext(TextView rightbottomtext) {
+		this.rightbottomtext = rightbottomtext;
+	}
+	
+	
+}
